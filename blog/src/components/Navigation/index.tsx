4ece0a3d5ff7b2ea1b50addd33394522799da73e
Robert <AQ_ROBY@YAHOO.COM>
2022-09-20 23:26:33 +0300

    More styling done to navbar

--- blog/src/components/Navigation/index.tsx
+++ blog/src/components/Navigation/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 const Navigation: React.FunctionComponent = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="secondary" expand="lg">
             <Container className="ml-2 w-25">
                 <Navbar.Brand href="/">
                     <img className="w-25 mr-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="logo" />
@@ -12,11 +11,11 @@ const Navigation: React.FunctionComponent = () => {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="mr-5" id="basic-navbar-nav">
-                    <Nav className="mr-5">
-                        <Nav.Link className="mr-2" href="/postss">
+                    <Nav variant="pills" className="mr-5">
+                        <Nav.Link style={{ color: 'black' }} className="mr-2" href="/posts">
                             Posts
                         </Nav.Link>
-                        <Nav.Link className="mr-4" href="/users">
+                        <Nav.Link style={{ color: 'black' }} className="mr-4" href="/users">
                             Users
                         </Nav.Link>
                     </Nav>
