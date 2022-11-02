ff91b2b0e185e7056a110f7c2e615b95dfff159f
Robert <AQ_ROBY@YAHOO.COM>
2022-11-02 21:42:14 +0200

    Main page looks and works as intended

--- blog/src/components/Layout.tsx
+++ blog/src/components/Layout.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-import data from '../mock-posts.json';
 import Header from './Header';
 import Navigation from './Navigation';
 
 const Layout: React.FunctionComponent = () => {
-    let dataSorted = data.sort((a, b) => (a.modified_at > b.modified_at ? 1 : -1));
-    let dataMapped: { id: number; title: string; contents: string; created_at: string; modified_at: string }[] = dataSorted;
-    const [posts, setPosts] = useState(dataMapped);
-
     return (
         <div>
             <Navigation />
--- blog/src/components/posts.tsx
+++ blog/src/components/posts.tsx
@@ -2,22 +2,24 @@ import PostRepo from '../services/repo';
 
 const Posts = () => {
     const repo = PostRepo.getInstance();
-    let asdsa = PostRepo.findAll();
 
-    function handleDate(date: Date) {}
+    function handleDate(date: Date) {
+        return date.toLocaleString();
+    }
+
     return (
         <div>
             {repo.instance.length === 0 ? (
                 <p>No posts found!</p>
             ) : (
                 repo.findAll().map((post: any) => (
-                    <div className="border mt-3 p-3 bgl" key={post.id}>
+                    <div className="border mt-3 mb-4 p-3 bgl" key={post.id}>
                         <div className="row p-2">
                             <div>
                                 <h2>{post.title}</h2>
                                 <h5>{post.contents}</h5>
-                                <p>Created at: {post.created_at}</p>
-                                <p>Modified at: {post.modified_at}</p>
+                                <p>Created at: {handleDate(new Date(post.created_at))}</p>
+                                <p>Modified at: {handleDate(new Date(post.modified_at))}</p>
                             </div>
                         </div>
                     </div>
